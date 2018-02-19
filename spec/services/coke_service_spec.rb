70d4da2ab387223fba308569f2ddd537e284c606
Paul Holden <pdmholden@gmail.com>
2018-02-18 17:31:48 -0800

    MOAR specs

--- spec/services/coke_service_spec.rb
+++ spec/services/coke_service_spec.rb
@@ -25,5 +25,23 @@ describe 'CokeService' do
         expect(service.service_error?).to be false
       end
     end
+
+    describe 'when there is an error message' do
+      before do
+       stub_request(:get, url).
+         with(  headers: {
+          'Connection'=>'close',
+          'Host'=>'devtestapiapp.herokuapp.com',
+          'User-Agent'=>'http.rb/3.0.0'
+           }).
+         to_return(status: 200, body: "{\"error\" : \"blah\"}", headers: {})
+
+        service.resource = HTTP.get(url)
+      end
+
+      it 'returns true' do
+        expect(service.service_error?).to be true
+      end
+    end
   end
 end
